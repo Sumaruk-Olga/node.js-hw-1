ba842599f95d10297dca5909752efbf0a9056102
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-12-13 03:28:56 +0200

    remove old code

--- contacts.js
+++ contacts.js
@@ -10,18 +10,14 @@ async function listContacts() {
 }
   
 async function getContactById(contactId) {
-    // const id =String(contactId);//for yargs
     const contacts = await listContacts();    
     const res = contacts.find(item=>item.id===contactId);
-    // const res = contacts.find(item=>item.id===id);//for yargs
     return res || null;
 }
   
 async function removeContact(contactId) {
-    // const id =String(contactId);//for yargs
     const contacts = await listContacts();
     const index = contacts.findIndex(item=>item.id===contactId);
-    // const index = contacts.findIndex(item=>item.id===id);//for yargs
     if (index===-1){ return null }
     const [res] = contacts.splice(index, 1);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
